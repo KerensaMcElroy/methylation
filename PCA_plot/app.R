7a1154b2a43f90e45b9ca0f91ab211b12b9ef444
KerensaMcElroy <kerensa.mcelroy@csiro.au>
2023-05-25 12:51:38 +1000

    functioning shiny app

--- PCA_plot/app.R
+++ PCA_plot/app.R
@@ -1,34 +1,40 @@
 library(shiny)
-library(DBI)
 library(tidyverse)
 library(readxl)
-library(mixOmics)
 library(janitor)
+
 
 # UI
 ui <- fluidPage(
   titlePanel("Chromosome Data Analysis"),
   sidebarLayout(
     sidebarPanel(
-      numericInput("chromosome", "Chromosome:", value = 29, min = 1, max = 99),
-      actionButton("runAnalysisBtn", "Run Analysis")
+      numericInput("chromosome", "Chromosome:", value = 1, min = 1, max = 29),
+      actionButton("runAnalysisBtn", "Run Analysis"),
+      br(),
+      br(),
+      selectInput("xAxis", "X-Axis:", choices = c("PC1", "PC2", "PC3", "PC4", "PC5", "PC6"), selected = "PC1"),
+      selectInput("yAxis", "Y-Axis:", choices = c("PC1", "PC2", "PC3", "PC4", "PC5", "PC6"), selected = "PC2"),
+      selectInput("colorFeature", "Color Feature:", choices = c("treatment", "days", "cohort",
+                                                                "room", "sodium", "potassium", "chloride", "bicarb", "anion_gap", "Mg",
+                                                                "glucose", "BOHB", "cholesterol", "urea", "creatinine", "U2C_ratio", "Ca",
+                                                                "Phos", "Ca2P_ratio", "T_protein", "albumin", "globulins", "A2G_ratio",
+                                                                "bilirubin", "ALP", "AST", "CK", "GTT", "GLDH", "glutamine", "glutamate"), selected = "treatment")
     ),
     mainPanel(
-      plotOutput("histPlot"),
-      sidebarPanel(
-        selectInput("xAxis", "X-Axis:", choices = c("PC1", "PC2", "PC3", "PC4"), selected = "PC1"),
-        selectInput("yAxis", "Y-Axis:", choices = c("PC1", "PC2", "PC3", "PC4"), selected = "PC2"),
-        selectInput("colorFeature", "Color Feature:", choices = c(), selected = NULL)
-      ),
-      plotOutput("pcaPlot")
+      plotOutput("pcaPlot", height = "80vh")
     )
   )
 )
 
 # Server
 server <- function(input, output) {
-  # Reactive value for storing tidy_pca
+  # Reactive values
   tidy_pca_data <- reactiveVal()
+  pca_plot <- reactiveVal()
+  color_features <- reactiveVal()
+  explained_variance <- reactiveVal()
+  
   
   # Perform the analysis for a given chromosome
   analyzeData <- function(chromosome) {
@@ -61,48 +67,88 @@ server <- function(input, output) {
       )) %>%
       pivot_longer(cols = 3:26, names_to = 'ID') %>%
       full_join(features, by = 'ID') %>%
-      pivot_wider(names_from = 'biochem', values_from = 'value')
+      pivot_wider(names_from = 'biochem', values_from = 'value') %>%
+      dplyr::filter(days %in% c(5, 12, 38))  # to match meth collection events
     
-   
-
-^    tidy_pca <- as_tibble(result.pca.multi[["x"]]) %>%
-      add_column(ID = pca_wide$ID, treatment = pca_wide$treatment) %>%
-      inner_join(biochem) %>%
+    pca_data <- read_csv(paste0("results/tidy_pca_", chromosome, ".csv"))
+    pca_data <- pca_data %>% mutate(ID = as.character(ID))
+    
+    explained_variance_data <- read_csv(paste0("results/explained_variance_", chromosome, ".csv"))
+    
+    explained_variance(explained_variance_data)
+    
+    # Print structure of pca_data
+    print("Structure of pca_data:")
+    str(pca_data)
+    
+    # Print structure of biochem
+    print("Structure of biochem:")
+    str(biochem)
+    
+    tidy_pca <- pca_data %>%
+      inner_join(biochem, by = c("ID", "treatment")) %>%
       mutate(days = as_factor(days))
     
-    tidy_pca_data(tidy_pca)  # Update the reactive value
+    print("Structure of tidy:")
+    str(tidy_pca)
+    
+    tidy_pca_data(tidy_pca)
+    pca_plot(NULL)
+    
   }
   
-
+  
   # Update axis and color feature choices when tidy_pca is available
   observeEvent(tidy_pca_data(), {
     output$xAxis <- renderUI({
-      selectInput("xAxis", "X-Axis:", choices = c("PC1", "PC2", "PC3", "PC4"), selected = "PC1")
+      selectInput("xAxis", "X-Axis:", choices = c("PC1", "PC2", "PC3", "PC4", "PC5", "PC6"), selected = "PC1")
     })
     
     output$yAxis <- renderUI({
-      selectInput("yAxis", "Y-Axis:", choices = c("PC1", "PC2", "PC3", "PC4"), selected = "PC2")
+      selectInput("yAxis", "Y-Axis:", choices = c("PC1", "PC2", "PC3", "PC4", "PC5", "PC6"), selected = "PC2")
     })
     
     output$colorFeature <- renderUI({
-      colorFeatures <- setdiff(colnames(tidy_pca_data()), c("PC1", "PC2", "PC3", "PC4"))
-      selectInput("colorFeature", "Color Feature:", choices = colorFeatures, selected = NULL)
+      choices <- c("treatment", "days", "cohort",
+                   "room", "sodium", "potassium", "chloride", "bicarb", "anion_gap", "Mg",
+                   "glucose", "BOHB", "cholesterol", "urea", "creatinine", "U2C_ratio", "Ca",
+                   "Phos", "Ca2P_ratio", "T_protein", "albumin", "globulins", "A2G_ratio",
+                   "bilirubin", "ALP", "AST", "CK", "GTT", "GLDH", "glutamine", "glutamate")
+      selectInput("colorFeature", "Color Feature:", choices = choices, selected = "treatment")
     })
+    
   })
-  
   # Perform analysis when "Run Analysis" button is clicked
   observeEvent(input$runAnalysisBtn, {
     analyzeData(input$chromosome)
   })
-  
+  observeEvent(input$chromosome, {
+    analyzeData(input$chromosome)
+  })
   # Render PCA plot
   output$pcaPlot <- renderPlot({
     tidy_pca <- tidy_pca_data()
     if (!is.null(tidy_pca)) {
-      PCA_plot <- ggplot(data = tidy_pca, mapping = aes_string(x = input$xAxis, y = input$yAxis, label = "ID", colour = input$colorFeature)) +
-        geom_label()
+      x_axis <- input$xAxis
+      y_axis <- input$yAxis
       
-      PCA_plot
+      pca_plot(NULL)  # Clear the previous plot
+      
+      plot_title <- paste0("PCA Analysis: ", x_axis, " vs ", y_axis)
+      
+      x_variance <- round(explained_variance()$value[explained_variance()$Component == as.numeric(str_extract(x_axis, "\\d+"))] * 100, 2)
+      y_variance <- round(explained_variance()$value[explained_variance()$Component == as.numeric(str_extract(y_axis, "\\d+"))] * 100, 2)
+      x_label <- paste0(x_axis, " (Explained Variance: ", x_variance, "%)")
+      y_label <- paste0(y_axis, " (Explained Variance: ", y_variance, "%)")
+      
+      color_feature <- input$colorFeature
+      pca_plot(
+        ggplot(data = tidy_pca, mapping = aes_string(x = x_axis, y = y_axis, label = "ID", fill = color_feature)) +
+          geom_label(color="black") +
+          labs(title = plot_title, x = x_label, y = y_label)
+      )
+      
+      pca_plot()
     }
   })
 }
